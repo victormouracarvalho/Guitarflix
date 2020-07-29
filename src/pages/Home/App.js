9316b69d161b84217f140b703027ce12576f2622
victo rmoura carvalho <48720986+victormouracarvalho@users.noreply.github.com>
2020-07-29 17:33:45 -0300

    17:33  9/07/2020

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import Menu from '../../componentes/Menu';
-
-
+import Menu from '../../componentes/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json';
+import Carousel from '../../componentes/Carousel';
+import Footer from '../../componentes/Footer';
+import BannerMain from '../../componentes/BannerMain';
 
 function Home() {
   return (
-    <div className="App">  
-     <Menu />
-    <h1> Bem vindo </h1>
-     Alguma coisa de teste x
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain 
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />      
+
+      <Footer />
     </div>
   );
 }
